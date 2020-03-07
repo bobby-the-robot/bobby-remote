aa45d196c8e99f8c8eaf860b3d0b86a7e69e6730
Anton Hlinisty <anton.hlinisty@gmail.com>
2020-03-07 18:55:25 +0300

    Add logger

--- src/main/java/bobby/remote/controller/DirectionController.java
+++ src/main/java/bobby/remote/controller/DirectionController.java
@@ -1,11 +1,13 @@
 package bobby.remote.controller;
 
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import bobby.remote.dto.MotionDto;
 
+@Slf4j
 @Controller
 public class DirectionController {
 
@@ -13,7 +15,7 @@ public class DirectionController {
     @MessageMapping("/server/direction")
     @SendTo("/client/direction")
     public MotionDto direction(MotionDto message) {
-        System.out.println(message);
+        log.info(message.toString());
         return message;
     }
 }
