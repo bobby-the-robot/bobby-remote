65575e8de4ecdafddb4ec21876dc652c94eda20c
Anton Hlinisty <anton.hlinisty@gmail.com>
2021-12-12 15:10:19 +0300

    Add integration tests

--- src/integration-test/groovy/bobby/remote/controller/MotionControllerIntegrationSpec.groovy
+++ src/integration-test/groovy/bobby/remote/controller/MotionControllerIntegrationSpec.groovy
@@ -26,7 +26,7 @@ class MotionControllerIntegrationSpec extends Specification {
     @SpringBean
     private Channel amqpChannel = Mock Channel
 
-    def "should send motion direction to message queue"() {
+    def 'should send motion direction to message queue'() {
         given:
         String direction = 'FORWARD'
         byte[] directionBytes = direction.bytes
@@ -41,6 +41,21 @@ class MotionControllerIntegrationSpec extends Specification {
         then:
         1 * amqpChannel.basicPublish(
                 '', MOTION_CONTROL_QUEUE_NAME, MessageProperties.TEXT_PLAIN, directionBytes)
+        0 * _
+    }
+
+    def 'should return bad request status'() {
+        given:
+        String direction = 'UNKNOWN'
+        String directionPayload = '{"direction": "UNKNOWN"}'
+
+        when:
+        mvc.perform(post("/move").accept(APPLICATION_JSON_VALUE)
+                    .contentType(APPLICATION_JSON_VALUE)
+                    .content(directionPayload))
+                .andExpect(status().isBadRequest())
+
+        then:
         0 * _
     }
 }
