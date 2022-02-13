009fa103fa39769c9fa7911d401a89a1a73482b6
Anton Hlinisty <anton.hlinisty@gmail.com>
2022-02-13 19:03:49 +0300

    Stream video via mjpeg

--- src/main/java/bobby/remote/controller/VideoController.java
+++ src/main/java/bobby/remote/controller/VideoController.java
@@ -1,15 +1,15 @@
 package bobby.remote.controller;
 
 import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.DeliverCallback;
+import com.rabbitmq.client.GetResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.OutputStream;
+import java.util.Date;
 
 import static bobby.remote.configuration.Constants.VIDEO_STREAMING_QUEUE_NAME;
 
@@ -21,38 +21,30 @@ public class VideoController {
 
     private final Channel channel;
 
-    /*@SneakyThrows
-    @GetMapping("/stream")
-    public void receive() {
-        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-            byte[] payloadBytes = delivery.getBody();
-            String payloadBase64 = Base64.getEncoder().encodeToString(payloadBytes);
-            messagingTemplate.convertAndSend("/topic/messages", payloadBase64);
-        };
-
-        channel.basicConsume(VIDEO_STREAMING_QUEUE_NAME, true, deliverCallback, consumerTag -> { });
-    }*/
-
     @SneakyThrows
     @GetMapping("/stream")
-    public void stream(HttpServletRequest request, HttpServletResponse response) {
+    protected void stream(HttpServletResponse response) {
         response.setContentType("multipart/x-mixed-replace; boundary=--BoundaryString");
         OutputStream outputStream = response.getOutputStream();
 
-        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-            byte[] payloadBytes = delivery.getBody();
-            // write the image and wrapper
-            outputStream.write((
-                    "--BoundaryString\r\n" +
-                            "Content-type: image/jpeg\r\n" +
-                            "Content-Length: " +
-                            payloadBytes.length +
-                            "\r\n\r\n").getBytes());
-            outputStream.write(payloadBytes);
-            outputStream.write(TWO_LINE_BYTES);
-            outputStream.flush();
-        };
+        while(true) {
+            GetResponse payload = channel.basicGet(VIDEO_STREAMING_QUEUE_NAME, true);
 
-        channel.basicConsume(VIDEO_STREAMING_QUEUE_NAME, true, deliverCallback, consumerTag -> { });
+            if (payload == null) {
+                Thread.sleep(10);
+            } else {
+                System.out.println(new Date());
+                byte[] body = payload.getBody();
+                outputStream.write((
+                        "--BoundaryString\r\n" +
+                                "Content-type: image/jpeg\r\n" +
+                                "Content-Length: " +
+                                body.length +
+                                "\r\n\r\n").getBytes());
+                outputStream.write(body);
+                outputStream.write(TWO_LINE_BYTES);
+                //outputStream.flush();
+            }
+        }
     }
 }
--- src/main/resources/static/video.js
+++
@@ -1,8 +0,0 @@
-const socket = new SockJS('/video');
-const stompClient = Stomp.over(socket);
-
-stompClient.connect({}, function(frame) {
-    stompClient.subscribe('/topic/messages', function(messageOutput) {
-        document.getElementById('response').src = 'data:image/jpg;base64,' + messageOutput.body;
-    });
-});
