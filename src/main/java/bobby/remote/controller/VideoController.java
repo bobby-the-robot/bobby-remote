2be20be31334feed14c76bde032fa9601b450650
Anton Hlinisty <anton.hlinisty@gmail.com>
2022-02-13 22:42:21 +0300

    Revert to msg template

--- src/main/java/bobby/remote/controller/VideoController.java
+++ src/main/java/bobby/remote/controller/VideoController.java
@@ -1,7 +1,6 @@
 package bobby.remote.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,18 +12,11 @@ import java.util.Base64;
 @RequiredArgsConstructor
 public class VideoController {
 
-    //private final SimpMessageSendingOperations messagingTemplate;
+    private final SimpMessageSendingOperations messagingTemplate;
 
     @PostMapping("/frames")
-    @SendTo("/topic/frames")
-    public String stream(@RequestBody byte[] frame) {
-        return Base64.getEncoder()
-                .encodeToString(frame);
-    }
-
-/*    @PostMapping("/frames")
     public void receive(@RequestBody byte[] bytes) {
         String payload = Base64.getEncoder().encodeToString(bytes);
         messagingTemplate.convertAndSend("/topic/frames", payload);
-    }*/
+    }
 }
