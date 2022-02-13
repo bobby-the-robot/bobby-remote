a3871c04103ac5efa41d50f7d42a1d4ec387cffb
Anton Hlinisty <anton.hlinisty@gmail.com>
2022-02-13 22:22:42 +0300

    Add messaging template

--- src/main/java/bobby/remote/controller/VideoController.java
+++ src/main/java/bobby/remote/controller/VideoController.java
@@ -2,6 +2,7 @@ package bobby.remote.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,10 +13,18 @@ import java.util.Base64;
 @RequiredArgsConstructor
 public class VideoController {
 
-    @PostMapping("/frames")
+    private final SimpMessageSendingOperations messagingTemplate;
+
+/*    @PostMapping("/frames")
     @SendTo("/topic/frames")
     public String stream(@RequestBody byte[] frame) {
         return Base64.getEncoder()
                 .encodeToString(frame);
+    }*/
+
+    @PostMapping("/frames")
+    public void receive(@RequestBody byte[] bytes) {
+        String payload = Base64.getEncoder().encodeToString(bytes);
+        messagingTemplate.convertAndSend("/topic/frames", payload);
     }
 }
