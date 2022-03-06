31676d297a6b5017e3465531e47c0c0dde834a8e
Anton Hlinisty <anton.hlinisty@gmail.com>
2022-03-06 17:55:04 +0300

    Log time in video controller

--- src/main/java/bobby/remote/controller/VideoController.java
+++ src/main/java/bobby/remote/controller/VideoController.java
@@ -6,6 +6,8 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.Date;
+
 @Controller
 @RequiredArgsConstructor
 public class VideoController {
@@ -13,6 +15,7 @@ public class VideoController {
     @MessageMapping("/client")
     @SendTo("/topic/frames")
     public String stream(@RequestBody String payload) {
+        System.out.println(new Date());
         return payload;
     }
 }
