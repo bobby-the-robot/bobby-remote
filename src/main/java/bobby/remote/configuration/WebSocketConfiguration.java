b145eeef4f87feb94f5d5dbef4e0c86ee77faf75
Anton Hlinisty <anton.hlinisty@gmail.com>
2022-03-06 16:42:19 +0300

    Boost message size limit

--- src/main/java/bobby/remote/configuration/WebSocketConfiguration.java
+++ src/main/java/bobby/remote/configuration/WebSocketConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -22,5 +23,11 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
         registry.addEndpoint("/client");
         registry.addEndpoint("/video");
         registry.addEndpoint("/video").withSockJS();
+    }
+
+    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
+        registration.setMessageSizeLimit(10 * 1024 * 1024); // default : 64 * 1024
+        registration.setSendTimeLimit(10 * 10000); // default : 10 * 10000
+        registration.setSendBufferSizeLimit(12 * 1024); // default : 512 * 1024
     }
 }
