23e1078108ca0dc748ea83114df990619412b1b3
Anton Hlinisty <anton.hlinisty@gmail.com>
2022-03-06 18:08:25 +0300

    Accept byte stream

--- src/main/java/bobby/remote/controller/VideoController.java
+++ src/main/java/bobby/remote/controller/VideoController.java
@@ -6,16 +6,24 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.Base64;
 import java.util.Date;
 
 @Controller
 @RequiredArgsConstructor
 public class VideoController {
 
-    @MessageMapping("/client")
+/*    @MessageMapping("/client")
     @SendTo("/topic/frames")
     public String stream(@RequestBody String payload) {
         System.out.println(new Date());
         return payload;
+    }*/
+
+    @MessageMapping("/client")
+    @SendTo("/topic/frames")
+    public String stream(@RequestBody byte[] bytes) {
+        System.out.println(new Date());
+        return Base64.getEncoder().encodeToString(bytes);
     }
 }
