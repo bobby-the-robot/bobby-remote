0a122b057e5f8036298f564b6462911aa2357e7e
Anton Hlinisty <anton.hlinisty@gmail.com>
2022-03-06 17:40:38 +0300

    Tackle websocket params

--- src/main/java/bobby/remote/configuration/WebSocketConfiguration.java
+++ src/main/java/bobby/remote/configuration/WebSocketConfiguration.java
@@ -1,37 +1,11 @@
 package bobby.remote.configuration;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
 import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
-@EnableWebSocketMessageBroker
-@RequiredArgsConstructor
-public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
-
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-    }
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/client");
-        registry.addEndpoint("/video");
-        registry.addEndpoint("/video").withSockJS();
-    }
-
-    public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
-        registration.setMessageSizeLimit(10 * 1024 * 1024); // default : 64 * 1024
-        registration.setSendTimeLimit(2048 * 10000); // default : 10 * 10000
-        registration.setSendBufferSizeLimit(200 * 512 * 1024); // default : 512 * 1024
-    }
+public class WebSocketConfiguration {
 
     @Bean
     public ServletServerContainerFactoryBean createServletServerContainerFactoryBean() {
