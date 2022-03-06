b23b4692daea67ea02bcdfc2b3d0ace3e3b1793b
Anton Hlinisty <anton.hlinisty@gmail.com>
2022-03-06 18:27:52 +0300

    Post frames via http

--- src/main/java/bobby/remote/controller/VideoController.java
+++ src/main/java/bobby/remote/controller/VideoController.java
@@ -4,37 +4,18 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Base64;
 import java.util.Date;
 
 @Controller
-//@RestController
 @RequiredArgsConstructor
 public class VideoController {
 
-/*    @MessageMapping("/client")
+    @MessageMapping("/client")
     @SendTo("/topic/frames")
     public String stream(@RequestBody String payload) {
         System.out.println(new Date());
-        return payload;
-    }*/
-
-/*    @MessageMapping("/client")
-    @SendTo("/topic/frames")
-    public String stream(@RequestBody byte[] bytes) {
-        System.out.println(new Date());
-        return Base64.getEncoder().encodeToString(bytes);
-    }*/
-
-    @PostMapping("/video/frames")
-    @SendTo("/topic/frames")
-    public String stream(@RequestBody byte[] bytes) {
-        System.out.println(new Date());
-        String payload = Base64.getEncoder().encodeToString(bytes);
         return payload;
     }
 }
