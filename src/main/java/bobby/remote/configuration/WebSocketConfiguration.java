47c86839eb29d82b3181f99df54af47026d69541
Anton Hlinisty <anton.hlinisty@gmail.com>
2022-03-06 17:34:54 +0300

    Tackle websocket params

--- src/main/java/bobby/remote/configuration/WebSocketConfiguration.java
+++ src/main/java/bobby/remote/configuration/WebSocketConfiguration.java
@@ -1,12 +1,14 @@
 package bobby.remote.configuration;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketTransportRegistration;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -27,7 +29,17 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
 
     public void configureWebSocketTransport(WebSocketTransportRegistration registration) {
         registration.setMessageSizeLimit(10 * 1024 * 1024); // default : 64 * 1024
-        registration.setSendTimeLimit(2028 * 10000); // default : 10 * 10000
+        registration.setSendTimeLimit(2048 * 10000); // default : 10 * 10000
         registration.setSendBufferSizeLimit(200 * 512 * 1024); // default : 512 * 1024
+    }
+
+    @Bean
+    public ServletServerContainerFactoryBean createServletServerContainerFactoryBean() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxTextMessageBufferSize(10 * 1024 * 1024);
+        container.setMaxSessionIdleTimeout(4096L * 4096L);
+        container.setAsyncSendTimeout(4096L * 4096L);
+        container.setMaxBinaryMessageBufferSize(10 * 1024 * 1024);
+        return container;
     }
 }
